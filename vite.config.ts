f91381e83dfd2302af1690bb0d381d9ef9ff886a
Liliane MAGRO <mathieu.piasco@gmail.com>
2026-08-20 09:41:50 +0200

    Corrige la page blanche sur GitHub Pages (base path absolu)
    Vite generait des chemins absolus (/assets/..., /favicon.ico) qui ne
    fonctionnent qu'a la racine d'un domaine. Le site est servi depuis un
    sous-chemin (piascwal.github.io/multix/), donc le JS/CSS pointait vers
    piascwal.github.io/assets/... (404) et l'app Preact ne montait jamais
    => page blanche. Passage en chemins relatifs (base: './' + liens
    favicon/manifest en ./ dans index.html), verifie via vite preview.

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
+  // Chemins relatifs : le site est servi depuis un sous-chemin sur GitHub
+  // Pages (https://<user>.github.io/multix/), pas depuis la racine du domaine.
+  base: './',
   plugins: [preact()],
 });
